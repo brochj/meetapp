05dbd8d1661184b24e48c897e3a15a9bb6510c1a
Oscar Broch <brochj@gmail.com>
2019-08-27 14:24:37 -0300

    feat: allow meetup update

--- src/app/controllers/MeetupController.js
+++ src/app/controllers/MeetupController.js
@@ -3,6 +3,46 @@ import * as Yup from 'yup';
 import Meetup from '../models/Meetup';
 
 class MeetupController {
+  async update(req, res) {
+    // data validation
+    const schema = Yup.object().shape({
+      title: Yup.string(),
+      description: Yup.string(),
+      date: Yup.date(),
+      location: Yup.string(),
+      file_id: Yup.number(),
+    });
+
+    schema.validate(req.body).catch(err => {
+      return res.status(400).json({ error: err.message });
+    });
+
+    const meetup = await Meetup.findByPk(req.params.id);
+
+    // Checks if the logged user is the meetup creator
+    if (meetup.user_id !== req.userId) {
+      return res
+        .status(401)
+        .json({ error: 'You do not have permission to change this meetup' });
+    }
+
+    // Checks if the date has passed
+    if (isBefore(parseISO(req.body.date), new Date())) {
+      return res.status(400).json({ error: 'Meetup date invalid' });
+    }
+
+    // Checks if the meeting has happened
+    if (meetup.past) {
+      return res
+        .status(400)
+        .json({ error: "Can't change a meeting that happened" });
+    }
+
+    await meetup.update(req.body);
+
+    return res.json(meetup);
+  }
+
   async store(req, res) {
     // data validation
     const schema = Yup.object().shape({
